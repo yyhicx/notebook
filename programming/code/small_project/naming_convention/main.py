4d96ed28124763d9bb41ed6dc26b4cc78a8d40d0
chnxish <1095219764@qq.com>
2023-09-02 22:39:15 +0800

    Add module docstring

--- programming/code/small_project/naming_convention/main.py
+++ programming/code/small_project/naming_convention/main.py
@@ -1,3 +1,8 @@
+# Copyright 2023 by chnxish. All Rights Reserved.
+#
+# Distributed under MIT license.
+# See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+"""Naming Convention."""
 import re
 import sys
 
